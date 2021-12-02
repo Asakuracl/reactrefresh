0cdded52e6d1bfeb10b5eab82a7c0c646a972c60
ludo <champi.ludo@tutanota.com>
2021-12-02 11:26:38 -0300

    update

--- reactproject1/src/components/Main.js
+++ reactproject1/src/components/Main.js
@@ -1,8 +1,21 @@
 import React from "react"
+import carrelage from "../images/carrelage.jpg"
 
 function Main() {
     return (    
-        <main>Main</main>
+        <main>
+            <img src={carrelage} className="main--img"/>
+            <h2 className="main--title">Entreprise</h2>
+            <h3>Entreprise tout corp d'état</h3>
+            <p>Carte de visite</p>
+            <div className="main--buttons">
+                <button>Email</button>
+                <button>Phone</button>
+            </div>
+            <h4>A propos</h4>
+
+            <h4>Compétences</h4>  
+        </main>
     )
 }
 
