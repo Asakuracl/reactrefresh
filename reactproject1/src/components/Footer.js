d7a3c1ed644ca88881264ebceabe89b25f899c79
Asakuracl <contact@neographe.org>
2021-12-02 21:51:14 -0300

    add link in icon

--- reactproject1/src/components/Footer.js
+++ reactproject1/src/components/Footer.js
@@ -3,9 +3,15 @@ import React from "react"
 function Footer() {
     return (    
         <footer>
-            <i class="fab fa-whatsapp"></i>
-            <i class="fab fa-instagram"></i>
-            <i class="far fa-envelope"></i>
+            <a href="#">
+                <i class="fab fa-whatsapp"></i>
+            </a>
+            <a href="https://www.instagram.com">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="mailto:someone@example.com">
+                <i class="far fa-envelope"></i>
+            </a>
         </footer>
     )
 }
