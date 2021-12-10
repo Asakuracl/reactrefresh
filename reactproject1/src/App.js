fecbdb3725767cbaf89cc80cfa9a9ffc6e628068
Asakuracl <contact@neographe.org>
2021-12-10 17:25:00 -0300

    remove props, add data

--- reactproject1/src/App.js
+++ reactproject1/src/App.js
@@ -2,26 +2,39 @@ import React from "react"
 import Header from "./components/Header" 
 import Main from "./components/Main"
 import Footer from "./components/Footer"
+carrelage from "../images/carrelage1.jpg"
 
 function App() {
     return (
         <div className="container">       
             <Header />
             <Main 
-                img={img}
-                mainTitle={mainTitle}
-                description={mainDescription}
-                webAddress ={webAddress}
-                buttonEmail={buttonEmail}
-                buttonPhone={buttonPhone}
+                img={carrelage}
+                mainTitle="Entreprise"
+                description="Entreprise tout corp d'état"
+                webAddress ="Carte de visite"
 
-                aboutFirst={aboutFirst}
-                aboutScd={aboutScd}
-                aboutTrd={aboutTrd}
+                aboutFirst="Devis gratuit et sous 48H."
+                aboutScd="+10 ans de métier et diplomé."
+                aboutTrd=""
 
-                skillFirst={skillFirst}
-                skillScd={skillScd}
-                skillTrd={skillTrd}
+                skillFirst="Pose carrelage neuf ou réhabilitation"
+                skillScd="Entreprise tout corp d'état"
+                skillTrd=""
+                // img={img}
+                // mainTitle={mainTitle}
+                // description={mainDescription}
+                // webAddress ={webAddress}
+                // buttonEmail={buttonEmail}
+                // buttonPhone={buttonPhone}
+
+                // aboutFirst={aboutFirst}
+                // aboutScd={aboutScd}
+                // aboutTrd={aboutTrd}
+
+                // skillFirst={skillFirst}
+                // skillScd={skillScd}
+                // skillTrd={skillTrd}
     
             />
             <Footer />
