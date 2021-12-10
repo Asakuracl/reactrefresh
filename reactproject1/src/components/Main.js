4a61e793604e04e9966f17dce4c73bb82ed26b73
Asakuracl <contact@neographe.org>
2021-12-10 17:35:19 -0300

    add props

--- reactproject1/src/components/Main.js
+++ reactproject1/src/components/Main.js
@@ -1,32 +1,30 @@
 import React from "react"
-import carrelage from "../images/carrelage1.jpg"
 
 function Test() {
-
     console.log("test ok")
 }
 
 
-function Main() {
+function Main(props) {
     return (    
         <main>
-            <img src={carrelage} className="main--img"/>
-            <h2 className="main--title">Entreprise</h2>
-            <h3>Entreprise tout corp d'état</h3>
-            <p>Carte de visite</p>
+            <img src={props.img} className="main--img"/>
+            <h2 className="main--title">{props.mainTitle}</h2>
+            <h3>{props.mainDescription}</h3>
+            <p>{props.webAddress}</p>
             <div className="main--buttons">
                 <button type="button">Email</button>
                 <button type="button" onClick={Test}>Phone</button>
             </div>
             <h4>A propos</h4>
             <ul>
-                <li>Devis gratuit et sous 48H.</li>
-                <li>+10 ans de métier et diplomé.</li>
+                <li>{props.aboutFirst}</li>
+                <li>{props.aboutScd}</li>
             </ul>
             <h4>Compétences</h4>
             <ul>
-                <li>Pose carrelage neuf ou réhabilitation</li>
-                <li>Entreprise tout corp d'état</li>
+                <li>{props.skillFirst}</li>
+                <li>{props.skillScd}</li>
             </ul>  
         </main>
     )
